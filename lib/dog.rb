c3680e1b325d8f4485d3629b3e0f28a0ed26d7dc
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 23:18:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -78,6 +78,14 @@ class Dog
     self
   end
 
+  def update
+    sql = <<-SQL
+    UPDATE dogs
+    SET name = ?, breed = ?
+    WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.id, self.name, self.breed)
+  end
 
 
 end
