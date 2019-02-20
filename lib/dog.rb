184bedd9e900a2617336a4ff5ae750ac13e2bfa3
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 23:02:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,4 +26,12 @@ class Dog
     @id = id
   end
 
+  def save
+    sql = <<-SQL
+    INSERT INTO dogs (name, breed) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
+
 end
