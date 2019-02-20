07ec7ec0d2bf9f2bba4d6a9e48080669171cc1e0
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 23:28:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -86,7 +86,7 @@ class Dog
     SET name = ?, breed = ?
     WHERE id = ?
     SQL
-    DB[:conn].execute(sql, @id, self.name, self.breed)
+    DB[:conn].execute(sql, self.name, self.breed)
     self
   end
 
