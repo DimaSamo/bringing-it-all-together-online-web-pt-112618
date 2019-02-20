ed024d582964dd2e2d07741161498e0b29195c0a
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 23:23:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,8 +39,9 @@ class Dog
     sql = <<-SQL
     SELECT * FROM dogs WHERE name = ?
     SQL
-    row = DB[:conn].execute(sql,name)[0]
-    if !row.empty?
+    rows = DB[:conn].execute(sql,name)
+    if !rows.empty?
+      row = rows = DB[:conn].execute(sql,name)[0]
       self.new(id: row[0], name:row[1], breed: row[2])
     end
   end
