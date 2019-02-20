1d97b22c77527628c4bf7fc7bf5282e977231bc6
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-20 23:00:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,6 +13,13 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
   def initialize (id: nil, name:, breed:)
     @name = name
     @breed = breed
